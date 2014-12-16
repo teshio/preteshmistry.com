d58e5732455c2bf5997e6f0b2ff8d9149be5cbd1
Pretesh <teshio@hotmail.com>
2014-12-16 21:57:50 +0000

    ga

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
-<html>
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<title>Pretesh Mistry - Home</title>
+</head>
 <body>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	<!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
 
-  ga('create', 'UA-11994419-1', 'auto');
-  ga('send', 'pageview');
+	<h1>helloworld...</h1>
 
-</script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-helloworld...
+	  ga('create', 'UA-11994419-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </body>
 </html>
