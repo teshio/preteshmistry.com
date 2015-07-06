22915c1bba8fce63fb48675d5e97c1f39dccbc30
Pretesh <teshio@hotmail.com>
2015-07-06 21:43:24 +0100

    tweaks

--- index.php
+++ index.php
@@ -5,18 +5,42 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Bootstrap 101 Template</title>
+        <title>Pretesh Mistry - Home</title>
         <link rel="icon" type="image/png" href="/images/favicon.png" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.min.css">       
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.3.0/animate.min.css">
         <style>
-
-            .tweet{
-                width: 33%;
-                padding:2px;
-
+            
+            .tweets{
+                padding: 10px;
             }
+
+            .tweet-sizer{
+                width: 30%;
+            }
+            .tweet{
+                width: 95%;
+                padding:10px;
+                background-color:#444;
+                border-radius: 3px;
+                margin: 5px;
+            }
+            
+            .tweet.ng-leave{
+                animation: fadeOut 2s;
+            }
+            @media screen and (min-width: 768px  ) {
+                .tweet { width: 30%; }
+            }
+            @media screen and (min-width: 992px ) {
+                .tweet { width: 30%; }
+            }
+            
+            @media screen and (min-width: 1200px) {
+                .tweet { width: 18%; }
+            }
+
 
         </style>
 
@@ -29,7 +53,7 @@
     <body ng-app="mainApp" ng-controller="mainCtrl">
 
         <nav class="navbar navbar-default navbar-static-top">
-            <div class="container2">
+            <div>
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
@@ -37,46 +61,63 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">What's happening right now!</a>
+                    <a class="navbar-brand" href="" ng-click="loadTweets()">What's happening right now!</a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                 </div><!--/.navbar-collapse -->
             </div>
         </nav>
 
+        <div class="container-fluid">
+            <button class="btn btn-primary" ng-click="loadTweets()">Refresh</button>
+        </div>
+
         <div class="tweets">
+            <div class="tweet-sizer"></div>
             <div class="tweet" ng-repeat="t in tweets.statuses">
-                <div class="well">
 
-                    <div class="container-fluid">
+                <div class="container-fluid">
 
-                        <div class="row-fluid">
-                            <div class="col-xs-3">
-                                <img class="img-responsive" ng-src="{{t.user.profile_image_url}}" />                                
-                            </div>
-                            <div class="col-xs-9">
-                                <p class="" ng-bind-html="twitterLinks(t.text)">
+                    <div class="row-fluid">
+                        
+                        <div class="media">
+                            
+                            <div class="media-left">
+                                <p>
+                                <img class="media-object" ng-src="{{t.user.profile_image_url}}" />                                                                
+                                    
                                 </p>
                             </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <p class="small">
-                                    <span class="text-info">{{t.user.screen_name}}</span>
-                                    &nbsp;|&nbsp;
-                                    <span class="text-success">{{t.created_at}}</span>
-
-                                </p>    
+                                                        <div class="media-body">
+                            <p class="" ng-bind-html="twitterLinks(t.text)"></p>
                             </div>
+
                         </div>
+                        
+                        
+                        <div class="col-xs-2">
+                        </div>
+                        <div class="col-xs-10">
+                        </div>
+                    </div>
 
+                    <div class="row-fluid">
+                        <div class="col-xs-12 text-right">
+                            <p class="small">
+                                <span class="text-info">{{t.user.screen_name}}</span>
+                                &nbsp;|&nbsp;
+                                <span class="text-success">{{ tweetDate(t) }}</span>
 
+                            </p>    
+                        </div>
+                    </div>
+
+                    <div class="row-fluid">
 
                         <img ng-repeat="mediaImage in t.entities.media" class="img-responsive" ng-src="{{mediaImage.media_url}}" />
                     </div>
-                </div>                
-            </div>
+                </div>
+            </div>                
 
         </div>
 
@@ -84,29 +125,53 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.1/angular-animate.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.1/angular-sanitize.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/masonry/3.3.1/masonry.pkgd.min.js"></script>        
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.1.8/imagesloaded.pkgd.min.js"></script>
 
-        <script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/masonry/3.3.1/masonry.pkgd.min.js"></script>        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.1/angular-sanitize.min.js"></script>
+        <script type="text/javascript">
                                     var app = angular.module('mainApp', ['ngAnimate', 'ngSanitize']);
                                     app.controller('mainCtrl', function ($scope, $http, $timeout) {
                                         $scope.test = 'sdf';
+                                        $scope.tweets = [];
 
-                                        $http.get('/twitter.php?q=exercise').success(function (data) {
-                                            $scope.tweets = data;
-                                            $timeout(function () {
-
-                                                $('.tweets').masonry({
-                                                    itemSelector: '.tweet',
-                                                });
-                                                console.log('here')
-                                            }, 0);
-
+                                        $scope.tweetsMason = {};
+                                        
+                                        $scope.tweetDate = function(t)
+                                        {
+                                            var dt = new Date(t.created_at);
+                                            return dt.toDateString();
+                                        };
 
 
 
-                                            console.log(data.statuses[0]);
-                                        });
+                                        $scope.loadTweets = function (repack) {
+                                            $scope.tweets = [];
+
+                                            $http.get('/twitter.php?q=worldnews').success(function (data) {
+                                                $scope.tweets = data;
+                                                $timeout(function () {
+
+                                                    if (!repack) {
+                                                        $scope.tweetsMason.masonry('destroy');
+                                                    }
+                                                    var grid = $('.tweets').masonry({
+                                                        itemSelector: '.tweet',
+                                                        percentPosition: true,
+                                                    });
+
+
+                                                    grid.imagesLoaded().progress(function () {
+                                                        grid.masonry('layout');
+                                                    });
+                                                    $scope.tweetsMason = grid;
+
+
+                                                }, 0);
+
+                                            });
+                                        };
+                                        $scope.loadTweets(true);
 
                                         $scope.twitterLinks = function (text)
                                         {
@@ -142,7 +207,6 @@
                                                     });
                                             return text;
                                         }
-
                                     });
         </script>
     </body>
