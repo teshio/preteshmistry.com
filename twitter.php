d2c471f8d26ff3f325ded8cf941a4b6ebffd4f7f
Pretesh <teshio@hotmail.com>
2015-07-10 00:01:19 +0100

    'tweaks'

--- twitter.php
+++ twitter.php
@@ -4,14 +4,24 @@ header("Access-Control-Allow-Origin: *");
 ini_set('display_errors', 1);
 
 require_once('TwitterAPIExchange.php');
+//
+//$settings = array(
+//    'oauth_access_token' => "183704754-4haXnrHppI6FwETgyYoPG6Ja8yVLV6ZtpgzgQVM",
+//    'oauth_access_token_secret' => "9IXRlbLmFW27VeTbQInIVjPmDUz4rBQsxYe94SRW9Ac",
+//    'consumer_key' => "LJrslT7mzx7zZjc5dRlOX6zi1",
+//    'consumer_secret' => "LBHpcjK7LfyhNMokAPikoUHmjZiHDDg1HdrsWCJyGNoMDWWxZJ"
+//);
 
-$settings = array(
-    'oauth_access_token' => "183704754-4haXnrHppI6FwETgyYoPG6Ja8yVLV6ZtpgzgQVM",
-    'oauth_access_token_secret' => "9IXRlbLmFW27VeTbQInIVjPmDUz4rBQsxYe94SRW9Ac",
-    'consumer_key' => "LJrslT7mzx7zZjc5dRlOX6zi1",
-    'consumer_secret' => "LBHpcjK7LfyhNMokAPikoUHmjZiHDDg1HdrsWCJyGNoMDWWxZJ"
-);
-
+if (file_exists('secrets.ini')) {
+    $settings = parse_ini_file("secrets.ini");
+} else {
+    $settings = array(
+        'oauth_access_token' => getenv('oauth_access_token'),
+        'oauth_access_token_secret' => getenv('oauth_access_token_secret'),
+        'consumer_key' => getenv('consumer_key'),
+        'consumer_secret' => getenv('consumer_secret')
+    );
+}
 
 $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
